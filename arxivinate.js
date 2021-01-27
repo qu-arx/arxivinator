9d15e81104cc717c07f3da7825207815fea9b54c
Louis Maddox <louismmx@gmail.com>
2021-01-27 10:43:09 +0000

    Updates after initial usage

--- arxivinate.js
+++ arxivinate.js
@@ -15,15 +15,16 @@ function parseArXivXML(xml_doc){
   var first_author = first_author_name_parts.join(" ");
   var title = xml_doc.querySelector("entry title").textContent;
   var url = xml_doc.querySelector("entry id").textContent;
+  var arx_id = url.split("/").pop()
   var updated = xml_doc.querySelector("entry updated").textContent;
-  var category = xml_doc.querySelector("category").getAttribute("term");
-  var formatted = "📄 " + first_author + " et al. (" + updated.split("-")[0] + ") " + title + " (arX⠶" + category + ") " + url;
+  var category = xml_doc.querySelector("category").getAttribute("term").split(".").join("⠶");
+  var formatted = "📄 " + first_author + " et al. (" + updated.split("-")[0] + ") " + title + " (arX⠶" + arx_id + " [" + category + "]) " + url;
   return formatted
 }
 
 function listInfo(){
   var search_term = document.getElementById("input").value;
-  var api_url = "http://export.arxiv.org/api/query?search_query=all:" + search_term + "&start=0&max_results=1"
+  var api_url = "https://export.arxiv.org/api/query?search_query=all:" + search_term + "&start=0&max_results=1"
   fetch(api_url)
 	.then(response => response.text())
 	.then(data => document.getElementById("responselist").innerHTML = parseArXivXML(parseXML(data)));
